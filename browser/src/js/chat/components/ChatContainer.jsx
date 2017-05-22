fd851588dfd09014871d2e7e4fba41cb1f75672c
Don Hansen <donhansen347@gmail.com>
2017-05-21 23:18:30 -0500

    connection :)

--- browser/src/js/chat/components/ChatContainer.jsx
+++ browser/src/js/chat/components/ChatContainer.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import io from 'socket.io-client';
 
 const ChatContainer = (props) => {
+    var socket = io(window.location.origin);
+
+    socket.on('connect', function () {
+        console.log('I have made a persistent two-way connection to the server!');
+    });
+
     return (
         <div>hello</div>
     );
